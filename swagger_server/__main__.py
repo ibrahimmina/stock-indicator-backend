7c1f1d149c376d411b7b2e115294243dc74e614f
ibrahimmina <minasamyibrahim@gmail.com>
2024-01-16 22:44:21 +0200

    third commit

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -5,7 +5,7 @@ import connexion
 from swagger_server import encoder
 
 
-def main():
+""" def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Stock Indicator Analysis Open API'}, pythonic_params=True)
@@ -14,3 +14,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+ """
+app = connexion.App(__name__, specification_dir='./swagger/')
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'Stock Indicator Analysis Open API'}, pythonic_params=True)
