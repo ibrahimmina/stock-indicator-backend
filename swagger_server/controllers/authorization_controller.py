f524bbbaa2b2198e7b88f7590b0968ac368337c6
ibrahimmina <minasamyibrahim@gmail.com>
2024-01-16 22:19:03 +0200

    updates

--- swagger_server/controllers/authorization_controller.py
+++ swagger_server/controllers/authorization_controller.py
@@ -1,9 +1,22 @@
 from typing import List
+from connexion.exceptions import OAuthProblem
+import os
+
 """
 controller generated to handled auth operation described at:
 https://connexion.readthedocs.io/en/latest/security.html
 """
+
+
+TOKEN_DB = {os.environ.get('api_key'): {"uid": 100}}
+
 def check_ApiKeyAuth(api_key, required_scopes):
-    return {'test_key': 'test_value'}
+
+    info = TOKEN_DB.get(api_key, None)
+
+    if not info:
+        raise OAuthProblem("Invalid token")
+
+    return info
 
 
--- swagger_server/controllers/overlays_controller.py
+++ swagger_server/controllers/overlays_controller.py
@@ -6,8 +6,29 @@ from swagger_server.models.ema import Ema  # noqa: E501
 from swagger_server.models.sma import Sma  # noqa: E501
 from swagger_server import util
 
+import pandas as pd
+import pandas_ta as ta
+import yfinance as yf
+from datetime import datetime, timedelta
 
-def calculate_bollinger_bands(symbol, start_date, period, length=None, standard_deviation=None):  # noqa: E501
+from polygon import RESTClient
+
+def get_historical_data_polygon(symbol,start_date,period,length):
+    input_start = datetime.strptime(start_date, "%Y-%m-%d")
+    start = input_start - timedelta(days=length+1)
+    end = input_start + timedelta(days=period)
+    client = RESTClient(trace=True)
+    stock = client.list_aggs(symbol,1,"day",start,end)
+    return stock
+
+def get_historical_data(symbol,start_date,period,length):
+    input_start = datetime.strptime(start_date, "%Y-%m-%d")
+    start = input_start - timedelta(days=length+1)
+    end = input_start + timedelta(days=period)
+    stock=yf.download(symbol, start=start, end=end)
+    return stock
+
+def calculate_bollinger_bands(symbol, start_date, period, length=5, standard_deviation=2):  # noqa: E501
     """An oscillator meaning that it operates between or within a set range of numbers or parameters..
 
      # noqa: E501
@@ -25,10 +46,21 @@ def calculate_bollinger_bands(symbol, start_date, period, length=None, standard_
 
     :rtype: Bollinger
     """
-    return 'do some magic!'
+
+    stock=get_historical_data(symbol, start_date,period,length)
+    bbands = stock.ta.bbands(close="Close", std=standard_deviation, length=length).dropna()
+    bbands = pd.merge(stock, bbands, left_index=True, right_index=True)
+    bbands.columns = bbands.columns.str.replace("_*.\d", "", regex=True)
+    bbands = bbands.drop(['BBB', 'BBP','Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1)
+    bbands['Date'] = pd.to_datetime(bbands.index.astype(str), format='%Y-%M-%d')
+    bbands['Date'] = bbands['Date'].dt.strftime('%Y-%M-%d')
+
+    output = Bollinger(bbands['Date'].values.tolist(), bbands['BBL'].values.tolist(), bbands['BBM'].values.tolist(), bbands['BBU'].values.tolist(), bbands['Close'].values.tolist())
+
+    return output
 
 
-def calculate_ema(symbol, start_date, period, length=None):  # noqa: E501
+def calculate_ema(symbol, start_date, period, length=5):  # noqa: E501
     """The average price over the specified period
 
      # noqa: E501
@@ -44,10 +76,20 @@ def calculate_ema(symbol, start_date, period, length=None):  # noqa: E501
 
     :rtype: Ema
     """
-    return 'do some magic!'
 
+    stock=get_historical_data(symbol, start_date,period,length)
+    ema = stock.ta.ema(close="Close", length=length).dropna()
+    outputdf = pd.merge(stock, ema, left_index=True, right_index=True)
+    jsondf = outputdf.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1)
+    jsondf.columns = jsondf.columns.str.replace("^EMA_*[0-9]*", "EMA", regex=True)
+    jsondf['Date'] = pd.to_datetime(jsondf.index.astype(str), format='%Y-%M-%d')
+    jsondf['Date'] = jsondf['Date'].dt.strftime('%Y-%M-%d')
 
-def calculate_sma(symbol, start_date, period, length=None):  # noqa: E501
+    output = Ema(jsondf['Date'].values.tolist(), jsondf['Close'].values.tolist(), jsondf['EMA'].values.tolist())
+
+    return output
+
+def calculate_sma(symbol, start_date, period, length=5):  # noqa: E501
     """The average price over the specified period
 
      # noqa: E501
@@ -63,4 +105,17 @@ def calculate_sma(symbol, start_date, period, length=None):  # noqa: E501
 
     :rtype: Sma
     """
-    return 'do some magic!'
+
+
+    stock=get_historical_data(symbol, start_date,period,length)
+    print(stock)
+    sma = stock.ta.sma(close="Close", length=length).dropna()
+    outputdf = pd.merge(stock, sma, left_index=True, right_index=True)
+    jsondf = outputdf.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1)
+    jsondf.columns = jsondf.columns.str.replace("^SMA_*[0-9]*", "SMA", regex=True)
+    jsondf['Date'] = pd.to_datetime(jsondf.index.astype(str), format='%Y-%M-%d')
+    jsondf['Date'] = jsondf['Date'].dt.strftime('%Y-%M-%d')
+
+    output = Sma(jsondf['Date'].values.tolist(), jsondf['Close'].values.tolist(), jsondf['SMA'].values.tolist())
+
+    return output
